cd863dec6233b90dbcbb64d8b8a7ebe4dcd6e3c6
Ohad Forman <ohadforman@mail.tau.ac.il>
2024-03-11 20:37:09 +0200

    store

--- Project_files/MyProject_code.py
+++ Project_files/MyProject_code.py
@@ -1,25 +1,19 @@
 from skidl import *
 
-# Define components
-resistor = Part('device', 'R', footprint='Resistor_SMD:R_0805')
-led = Part('device', 'LED', footprint='LED:LED_0805')
-buzzer = Part('device', 'Buzzer', footprint='Buzzer:Buzzer_SMD')
-capacitor = Part('device', 'C', footprint='Capacitor_SMD:C_0805')
+# Create the parts
+led = Part('device', 'LED')
 
-# Connections
+# Create the circuit
 net_5v = Net('5V')
 net_gnd = Net('GND')
-net_sig = Net('SIG')
 
-res1 = resistor(value='1k', ref='R1', pin_nums=['1', '2'])
-led1 = led(ref='LED1', pin_nums=[1, 2])
-buzzer1 = buzzer(ref='BUZZER1', pin_nums=[1, 2])
-cap1 = capacitor(value='100n', ref='C1', pin_nums=['1', '2'])
+led[1] += net_5v
+led[2] += net_gnd
 
-buzzer1[2] += net_5v & net_gnd
-led1[2] += net_5v & net_gnd
-res1[1,2] += net_5v
-cap1[1,2] += net_gnd
+# Define waveforms for blinking
+net_blink = Net('BLINK')
+Vpulse(net_blink, 0, 1, 1ms, 1ms)
 
-# Generate the netlist and print it to the console
-generate_netlist()
+led[1] & net_blink & Net()
+
+Circuit('blinking_led', led, net_5v, net_gnd, net_blink)
